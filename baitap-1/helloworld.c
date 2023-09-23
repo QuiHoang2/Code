baa163287cf25bbe2426d5e41e7c13d7137a5499
Qui <hoangqui11125@gmail.com>
2023-09-23 13:31:25 +0700

    complete week 1

--- baitap-1/helloworld.c
+++ baitap-1/helloworld.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
-int main()
+int main() 
 {
 	int a, b, c, max;
-	printf("Nhập a,b,c = ");
+	printf("Nhap a, b, c = ");
 	scanf("%d %d %d", &a, &b, &c);
 	max = a;
 	if (b > max) max = b;
 	if (c > max) max = c;
-	printf("max = %d", max);
 }
